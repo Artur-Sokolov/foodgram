45dea843de523e5606f7ade3c1e394487dfbf30b
Artur Sokolov <roshan9416@gmail.com>
2025-08-03 16:10:12 -0400

    123

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR /'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-default')
