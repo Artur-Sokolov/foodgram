42e9a00459591685ce43fda58a752b4d3fe131c0
Artur Sokolov <roshan9416@gmail.com>
2025-08-07 20:06:38 -0400

    21

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-default')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING:don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost,http://127.0.0.1').split(',')
