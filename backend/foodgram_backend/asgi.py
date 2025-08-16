41ee54161c04ebf412c7ec36ea3c9454397a14b5
Artur Sokolov <roshan9416@gmail.com>
2025-08-15 20:23:34 -0400

    432

--- backend/foodgram_backend/asgi.py
+++ backend/foodgram_backend/asgi.py
@@ -1,7 +1,7 @@
 """
 ASGI config for foodgram_backend project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level viable named ``application``.
 
 Formoreinformation on this file, see
 """
